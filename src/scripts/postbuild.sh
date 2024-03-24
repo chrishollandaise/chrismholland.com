258d99e43cd2a58bf0113fcd43e02035b79925c0
chrishollandaise <chrishollandaise@gmail.com>
2024-03-23 18:26:57 -0700

    now compatible gnu regex flavor

--- src/scripts/postbuild.sh
+++ src/scripts/postbuild.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 # Unsure currently if this can be done via astro integration (build done hook), but it works.
+tee build-output.txt && cat build-output.txt
 
-# BUILD_TIME=$(grep -o '\d\+\spage.\+')
+BUILD_TIME=$(grep -o '[0-9]\spage(s)\sbuilt.*')
 
-# echo $BUILD_TIME
+CURRENT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
+CURRENT_COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
 
-# ls dist
-
-# CURRENT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
-# CURRENT_COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
-
-# for FILE in $(find dist/ -name '*.html'); do
-#     sed -i.bak "s/%X_SECONDS%/$BUILD_TIME/g" "$FILE"
-#     sed -i.bak "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$FILE"
-#     sed -i.bak "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$FILE"
-#     rm "${FILE}.bak"
-# done
+for FILE in $(find dist/ -name '*.html'); do
+    sed -i.bak "s/%X_SECONDS%/$BUILD_TIME/g" "$FILE"
+    sed -i.bak "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$FILE"
+    sed -i.bak "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$FILE"
+    rm "${FILE}.bak"
+done
 
 echo "[postbuild] Finished post build."
