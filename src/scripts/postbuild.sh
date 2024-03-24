c33750ae91747a2f89ee0abfe89d54681ea60106
chrishollandaise <chrishollandaise@gmail.com>
2024-03-23 18:02:33 -0700

    make command blank

--- src/scripts/postbuild.sh
+++ src/scripts/postbuild.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # Unsure currently if this can be done via astro integration (build done hook), but it works.
 
-BUILD_TIME=$(grep -o '\d\+\spage.\+')
+# BUILD_TIME=$(grep -o '\d\+\spage.\+')
 
-echo $BUILD_TIME
+# echo $BUILD_TIME
 
-ls dist
+# ls dist
 
-CURRENT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
-CURRENT_COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
+# CURRENT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
+# CURRENT_COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
 
-for FILE in $(find dist/ -name '*.html'); do
-    sed -i.bak "s/%X_SECONDS%/$BUILD_TIME/g" "$FILE"
-    sed -i.bak "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$FILE"
-    sed -i.bak "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$FILE"
-    rm "${FILE}.bak"
-done
+# for FILE in $(find dist/ -name '*.html'); do
+#     sed -i.bak "s/%X_SECONDS%/$BUILD_TIME/g" "$FILE"
+#     sed -i.bak "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$FILE"
+#     sed -i.bak "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$FILE"
+#     rm "${FILE}.bak"
+# done
 
 echo "[postbuild] Finished post build."
