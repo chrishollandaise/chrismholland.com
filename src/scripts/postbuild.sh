6a370f7c9e2ef6a849afe4962568b2cae14413dd
chrishollandaise <chrishollandaise@gmail.com>
2024-03-23 17:31:05 -0700

    make postbuild.sh portable

--- src/scripts/postbuild.sh
+++ src/scripts/postbuild.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # Unsure currently if this can be done via astro integration (build done hook), but it works.
 
-BUILD_TIME=`grep -o '\d\spage.*'`
-CURRENT_COMMIT_HASH_FULL=`git rev-parse HEAD`
-CURRENT_COMMIT_HASH_SHORT=`git rev-parse --short HEAD`
+BUILD_TIME=$(grep -o '\d\+\spage.\+')
 
+echo $BUILD_TIME
 
-for i in $(find dist/* | grep .html); do
-  sed -i "s/%X_SECONDS%/$BUILD_TIME/g" "$i"
-  sed -i "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$i"
-  sed -i "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$i"
+CURRENT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
+CURRENT_COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
+
+for FILE in $(find dist/ -name '*.html'); do
+    sed -i.bak "s/%X_SECONDS%/$BUILD_TIME/g" "$FILE"
+    sed -i.bak "s/%SHORT_COMMIT_HASH%/$CURRENT_COMMIT_HASH_SHORT/g" "$FILE"
+    sed -i.bak "s/%FULL_COMMIT_HASH%/$CURRENT_COMMIT_HASH_FULL/g" "$FILE"
+    rm "${FILE}.bak"
 done
 
-echo "\n[postbuild] Finished post build."
+echo "[postbuild] Finished post build."
